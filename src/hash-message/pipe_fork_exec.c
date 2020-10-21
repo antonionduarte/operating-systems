96f11af266cbbce4d645d0ea946260329b4d3311
António Duarte <an.duarte@campus.fct.unl.pt>
2020-10-22 00:10:15 +0100

    Made pipe system

--- src/hash-message/pipe_fork_exec.c
+++ src/hash-message/pipe_fork_exec.c
@@ -8,36 +8,48 @@
 #include <fcntl.h>
 
 void usage (const char *prog) {
-    printf ("Usages: %s [file_path]\n", prog);
-        exit (1);
+  printf ("Usages: %s [file_path]\n", prog);
+      exit (1);
 }
 
 int main (int argc, char *argv[]) {
-    // Check there is one and only one argument
-    if (argc < 1 || argc > 2) {
-        usage (argv[0]);
-    }
+  // Check there is one and only one argument
+  if (argc < 1 || argc > 2) {
+      usage (argv[0]);
+  }
 
-    int pid = fork();
+  int pid = fork(); // fork of parent process
+  int fd[2]; // array with read and write extremities of pipe
+  pipe(fd); // create the communication pipe
 
-    switch (pid) {
-      case -1: // error
-        perror("fork");
-        exit(1);
-        break;
-      case 0: // child process
-        // execv -> hash_words
-      default: // parent process
-        // execv -> execute split_words
-    }
+  switch (pid) {
+    case -1: // error
+      perror("fork");
+      exit(1);
+      break;
+    case 0: // child process (executes hash_words)
+      close(stdin);
+      dup(fd[0]);
+      close(fd[1]);
+      close(fd[0]);
+      execv("hash_words", NULL);
+      break;
+    default: // parent process (executes split_words)
+      close(stdout);
+      dup(fd[1]);
+      close(fd[1]);
+      close(fd[0]);
+      execv("split_words", argv);
+      break;
+  }
 
-    //----------------------------------------
-    // Do your stuff here
-    //
-    // create pipe, fork and redirect I/O to/from pipe,
-    // parent execs one of "split_words" or "hash_words" 
-    // and the child execs the other one
-    //----------------------------------------
-    
-    exit (0);
+  //----------------------------------------
+  // Do your stuff here
+  //
+  // create pipe, fork and redirect I/O to/from pipe,
+  // parent execs one of "split_words" or "hash_words" 
+  // and the child execs the other one
+  //----------------------------------------
+  
+  exit (0);
 }
